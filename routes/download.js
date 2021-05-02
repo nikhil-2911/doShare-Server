f79d8502cc0584f7399acbcf8f4b018b9dd9e60e
nikhil-2911 <nikhilxmr5@gmail.com>
2021-05-02 13:43:39 +0530

    removing download error

--- routes/download.js
+++ routes/download.js
@@ -6,7 +6,7 @@ router.get('/:uuid', async (req, res) => {
      const file = await File.findOne({ uuid : req.params.uuid });
      if(!file){
          return res.render('download', {error : 'Link has been expired'});
-     }c
+     }
 
      const filePath = `${__dirname}/../${file.path}`;
      res.download(filePath);
